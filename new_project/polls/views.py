3d827b76009281406071b8c731418476c135c615
Yurazasimuk <118778985+Yurazasimuk@users.noreply.github.com>
2025-04-25 14:49:59 +0300

    Add files via upload

---
+++ new_project/polls/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistrationForm
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # автоматичний вхід після реєстрації
+            return redirect('home')  # редирект на домашню сторінку
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
